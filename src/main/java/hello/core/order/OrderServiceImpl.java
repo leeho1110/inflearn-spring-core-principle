ae0f8dd2d791356c79b34b0063a8befa36b7dd4e
Ho-Lee <ho.sol.lee@gmail.com>
2021-12-09 21:44:19 +0900

    feat: 현 할인정책의 문제점과 개선
    할인 정책을 변경하기 위해선 OrderService의 구현체를 변경해야 하므로 DIP를 위반하고 있다.
    즉, OrderServiceImple은 DIscountPolicy 인터페이스 뿐만 아니라 FixDiscountPolicy인 구체 클래스도 함께 의존하고 있다.
    따라서 우리는 DIP를 위반하지 않도록 추상(인터페이스)에만 의존하도록 의존관계를 변경해야한다.
    
    또한 할인 정책을 Fix -> Rate로 변경하는 순간 OrderServiceImpl을 변경한다. 즉 OCP도 위반한다.
    변경을 하려면 할인 관련만 변경해야 하는데 주문 객체가 변경되었기 때문이다.
    
    따라서 우리는 인터페이스에만 의존관계를 갖도록 설정하고 누군가가 구현객체를 넣도록 변경하려 한다.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,14 +1,19 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 	private final MemberRepository memberRepository = new MemoryMemberRepository();
-	private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+	// Fix -> Rate 로 할인정책 변경할 때 문제점 발생
+	// private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+	// private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+	// OCP, DIP를 지키고 NPE를 막기 위해서 discountPolicy에 구현객체를 누군가 넣어줘야 한다.
+	private DiscountPolicy discountPolicy;
 
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
