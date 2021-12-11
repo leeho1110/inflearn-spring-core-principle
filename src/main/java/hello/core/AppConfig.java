8df0cc445201acc19e8b72437830ef4aa3916210
Ho-Lee <ho.sol.lee@gmail.com>
2021-12-11 15:00:20 +0900

    feat: 스프링 컨테이너 생성 및 빈 조회

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
@@ -8,24 +11,25 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
 
-	// 역할
+	@Bean
 	public MemberService memberService() {
 		return new MemberServiceImpl(memberRepository());
 	}
 
-	// 구현
-	private MemoryMemberRepository memberRepository() {
+	@Bean
+	public MemoryMemberRepository memberRepository() {
 		return new MemoryMemberRepository();
 	}
 
-	// 역할
+	@Bean
 	public OrderService orderService() {
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
 
-	// 구현
+	@Bean
 	public DiscountPolicy discountPolicy() {
 		return new FixDiscountPolicy();
 	}
