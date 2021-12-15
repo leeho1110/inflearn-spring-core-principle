8cf2df529966968feff07b561ade18eb4171fa6c
Ho-Lee <ho.sol.lee@gmail.com>
2021-12-15 21:39:57 +0900

    feat: 컴포넌트 스캔(@ComponentScan)과 @Component, @Autowired

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,9 +1,13 @@
 package hello.core.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 	private final MemberRepository memberRepository;
 
@@ -14,6 +18,7 @@ public class OrderServiceImpl implements OrderService {
 	// OCP, DIP를 지키고 NPE를 막기 위해서 discountPolicy에 구현객체를 누군가 넣어줘야 한다.
 	private final DiscountPolicy discountPolicy;
 
+	@Autowired
 	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
 		this.memberRepository = memberRepository;
 		this.discountPolicy = discountPolicy;
