6771a6a06284da1e924be3ca97f2b779e37d9ab1
Ho-Lee <ho.sol.lee@gmail.com>
2021-12-21 21:45:26 +0900

    feat: Lombok @RequiredArgsConstructor 를 통한 생성자 자동 생성

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,28 +1,19 @@
 package hello.core.order;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
-	private final MemberRepository memberRepository;
-
-	// Fix -> Rate 로 할인정책 변경할 때 문제점 발생
-	// private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-	// private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
 	// OCP, DIP를 지키고 NPE를 막기 위해서 discountPolicy에 구현객체를 누군가 넣어줘야 한다.
+	private final MemberRepository memberRepository;
 	private final DiscountPolicy discountPolicy;
-
-	@Autowired
-	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-		this.memberRepository = memberRepository;
-		this.discountPolicy = discountPolicy;
-	}
 
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
