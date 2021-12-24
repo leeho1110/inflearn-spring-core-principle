288af74d9e173eadf941da67612f0703ec8578fd
Ho-Lee <ho.sol.lee@gmail.com>
2021-12-24 14:30:40 +0900

    feat: 인터페이스를 Bean 설정 정보(메소드)로 대체
    메서드 이름을 자유롭게 줄 수 있고, 스프링 빈이 스프링 코드(Spring 인터페이스)에 의존하지 않는다.
    코드가 아니라 설정 정보를 사용하기 때문에 코드를 고칠 수 없는 외부 라이브러리에도 초기화, 종료 메소드를 적용할 수 있다.

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,11 +1,6 @@
 package hello.core.lifecycle;
 
-import javax.security.auth.Destroyable;
-
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
 	private String url;
 
@@ -32,16 +27,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
 		System.out.println("disconnect: " + url);
 	}
 
-	@Override
-	public void afterPropertiesSet() throws Exception {
+	public void init() {
 		System.out.println("NetworkClient.afterPropertiesSet");
 		// 빈 의존관계 주입이 끝난 뒤 실행
 		connect();
 		call("초기화 연결 메시지");
 	}
 
-	@Override
-	public void destroy() throws Exception {
+	public void close() {
 		System.out.println("NetworkClient.destroy");
 		// 빈이 소멸되기 직전에 실행
 		disconnect();
