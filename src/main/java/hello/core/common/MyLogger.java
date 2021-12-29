dccd7e377aa6cd5d6d6e00e3fc446d61cc573382
Ho-Lee <ho.sol.lee@gmail.com>
2021-12-29 21:22:06 +0900

    feat: @Bean (scope=Request)
    스프링 컨테이너가 올라와서 Bean을 생성하려는 시점에서는 myLogger 에게 의존성 주입을 하려는 Bean은 생성되지 않아 오류가 난다. 그 이유는 Bean의 라이프사이클이 Request이기 때문이다.

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,38 @@
+package hello.core.common;
+
+import java.util.UUID;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "request")
+// HTTP Request 당 하나씩 생성되고, HTTP Request이 끝나는 시점에 소멸된다.
+public class MyLogger {
+
+	private String uuid;
+	private String requestURL;
+
+	public void setRequestURL(String requestURL) {
+		this.requestURL = requestURL;
+	}
+
+	public void log(String message) {
+		System.out.println("[" + uuid + "]" + "[" + requestURL + "]" + message);
+	}
+
+	@PostConstruct
+	public void init() {
+		uuid = UUID.randomUUID().toString();
+		System.out.println("[" + uuid + "] request scope bean CREATE:" + this);
+	}
+
+	@PreDestroy
+	public void close() {
+		System.out.println("[" + uuid + "] request scope bean CLOSE:" + this);
+	}
+
+}
