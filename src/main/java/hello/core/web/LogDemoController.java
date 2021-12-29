90d0a2c6a2cd1e515300d9ad72b7f6b17048f78e
Ho-Lee <ho.sol.lee@gmail.com>
2021-12-29 22:05:24 +0900

    feat: ObjectProvider 를 활용한 Bean 호출 지연
    ObjectProvider 덕분에 ObjectProvider.getObject()를 호출하는 시점까지 request scope를 갖는 Bean을 생성(스프링 컨테이너에게 요청)하는 것을 지연시킬  수 있었다.
    
    동시에 여러 요청이 와도 Bean 객체(MyLogger)를 따로 관리해준다. 같은 HTTP 요청이면 동일한 Bean을 반환해준다. Sleep을 통해서 확인해봤다.

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,15 +15,18 @@ import lombok.RequiredArgsConstructor;
 public class LogDemoController {
 
 	private final LogDemoService logDemoService;
-	private final MyLogger myLogger;
+	private final ObjectProvider<MyLogger> myLoggerObjectProvider;
 
 	@RequestMapping("log-demo")
 	@ResponseBody
-	public String logDemo(HttpServletRequest request){
-		String requestURL = request.getRequestURI().toString();
+	public String logDemo(HttpServletRequest request) throws InterruptedException {
+		String requestURL = request.getRequestURL().toString();
+
+		MyLogger myLogger = myLoggerObjectProvider.getObject();
 		myLogger.setRequestURL(requestURL);
 
 		myLogger.log("controller test");
+		Thread.sleep(2000);
 		logDemoService.logic("testId");
 
 		return "OK";
